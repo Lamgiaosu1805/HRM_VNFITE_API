0e92c12aed5082418ab69eeb2cab80aeb11c15bf
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-06-10 17:22:50 +0700

    add route cty

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const authRouter = require('./auth')
+const companyRouter = require('./company')
 
 const api_ver = "/api/v1"
 const route = (app) => {
     app.use(`${api_ver}/auth`, authRouter)
+    app.use(`${api_ver}/company`, companyRouter)
 }
 
 module.exports = route;
